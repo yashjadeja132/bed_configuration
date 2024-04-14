9d2fe9395adffbc1f697907484328f0582c5a453
keybrains <kbtplseo@gmail.com>
2024-04-14 12:47:56 +0530

    yash

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Container } from "./styles";
 import { BrowserRouter as Router } from "react-router-dom";
 import { NavHashLink, HashLink } from "react-router-hash-link";
 import { useState } from "react";
-import Resume from "../../assets/sahils resume.pdf";
+import Resume from "../../assets/YashrajsinhJadejaResume.pdf";
 
 export function Header() {
   const [isActive, setActive] = useState(false);
